997e36002ef6c8dc54269b266374ede1c82b6f6b
gaurangrshah <gaurang.r.shah@gmail.com>
2020-01-05 13:47:11 -0500

    adds envvars

--- gatsby-lesson-2/gatsby-config.js
+++ gatsby-lesson-2/gatsby-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -25,6 +30,14 @@ module.exports = {
         // path: `${__dirname}/src/pages/`,
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACEID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ]
